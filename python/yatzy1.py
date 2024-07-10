7b7d06824343752a993823c2ca7d93f1abedc2cf
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2024-07-10 19:25:51 +0200

    WIP

--- python/yatzy1.py
+++ python/yatzy1.py
@@ -1,19 +1,21 @@
 from dataclasses import dataclass
 from typing import List
 
+
 @dataclass(frozen=True)
 class Dice:
     values: List[int]
 
+
 class Yatzy:
 
-    def __init__(self, d1=0, d2=0, d3=0, d4=0, _5=0):
+    def __init__(self, d1=0, d2=0, d3=0, d4=0, d5=0):
         self.dice = [0] * 5
         self.dice[0] = d1
         self.dice[1] = d2
         self.dice[2] = d3
         self.dice[3] = d4
-        self.dice[4] = _5
+        self.dice[4] = d5
 
     @staticmethod
     def chance(d1, d2, d3, d4, d5):
